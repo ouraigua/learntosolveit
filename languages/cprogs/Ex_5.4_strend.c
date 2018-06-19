901d3b6ee0457fe66e573cf679dfc6410e904696
ouraigua <ouraigua@icloud.com>
2018-06-19 02:36:51 +0100

    alternative minimal suggestion for func  "strend"
    int strend(char *s, char *t) {
    	char *p = t;
    	while (*t++ != '\0');
    	while (*s++ != '\0');
    	while(*--s == *--t && t >= p);
    	return ++t == p;
    }

--- languages/cprogs/Ex_5.4_strend.c
+++ languages/cprogs/Ex_5.4_strend.c
@@ -1,72 +1,23 @@
-#include<stdio.h>
-#define MAXLINE 1000
+/*
+	Write the function strend(s,t), which returns 1 if the string t occurs at the
+	end of the string s, and zero otherwise.
+*/
 
-int mgetline(char s[],int max);
-int strend(char *s,char *t);
-int mystrlen(char *t);
+#include <stdio.h>
 
-int main(void)
-{
-    char s[MAXLINE],t[MAXLINE];
-    int ret;
-    mgetline(s,MAXLINE);
-    mgetline(t,MAXLINE);
-    ret = strend(s,t);
-    printf("%d",ret);
-    return 0;
+int strend(char *, char *);
+
+int main(int argc, char const *argv[]) {
+	char *s = "hello world!";
+	char *t = "world!";
+	printf("output: %d\n", strend(s, t));
+	return 0;
 }
 
-int mgetline(char s[],int lim)
-{
-    int c,i;
-    
-    for(i=0;i<lim-1 && ((c=getchar())!=EOF) && c!='\n';++i)
-        s[i]=c;
-
-    if(c=='\n')
-    {
-        s[i]=c;
-        ++i;
-    }
-    s[i]='\0';
-
-    return i;
-}
-
-int strend(char *s,char *t)
-{
-    int len;
-    len=mystrlen(t);
-    while(*s!='\0')
-        ++s;
-    --s;
-
-    while(*t!='\0')
-        ++t;
-            
-    --t;
-    while(len > 0)
-    {
-        if(*t==*s)
-        {
-            --t;
-            --s;
-            --len;
-        }
-        else
-            return 0;
-    }
-    if( len == 0)
-        return 1;
-}
-
-int mystrlen(char *t)
-{
-    char *p;
-    p=t;
-
-    while(*p!='\0')
-        ++p;
-
-    return p-t;
+int strend(char *s, char *t) {
+	char *p = t;
+	while (*t++ != '\0');
+	while (*s++ != '\0');
+	while(*--s == *--t && t >= p);
+	return ++t == p;
 }
