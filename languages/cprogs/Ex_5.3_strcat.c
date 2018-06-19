06e7803dd8a2f1bba23799ad50444bf5550f50b2
ouraigua <ouraigua@icloud.com>
2018-06-19 02:45:45 +0100

    Update Ex_5.3_strcat.c

--- languages/cprogs/Ex_5.3_strcat.c
+++ languages/cprogs/Ex_5.3_strcat.c
@@ -44,13 +44,8 @@ int mgetline(char s[],int lim)
 
 void mystrcat(char *s,char *t)
 {
-	while(*s!='\0')
-	    s++;
-	s--; 		     /* goes back to \0 char */
-	while((*s=*t)!='\0')
-	{	
-	    s++;
-	    t++;
-	}
+	while(*s != '\0')
+		s++;
+	while((*s++ = *t++) != '\0');
 }
 
